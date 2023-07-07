5f4093bc816eba4120952d5587eda0f95ae33279
=Xavier <github@tttp.eu>
2023-07-07 11:44:32 +0200

    WIP trust LC

---
+++ src/crm/trust-lc.ts
@@ -0,0 +1,56 @@
+import _ from "lodash";
+import {
+  CRM,
+  CRMType,
+  ActionMessage,
+  handleResult,
+  ProcaCampaign,
+  pause,
+} from "../crm";
+import { writeFileSync } from "fs";
+import { formatAction, handleConsent } from "./trust-lc/data";
+import { postAction, verification} from "./trust-lc/client";
+
+/*
+ * A debug CRM that displays the messages and events in the log
+ *
+ */
+
+class TrustCRM extends CRM {
+
+
+  handleContact = async (
+    message: ActionMessage
+  ): Promise<handleResult | boolean> => {
+    const camp = await this.campaign(message.campaign);
+    console.log("message",message.actionId, message.campaign?.title, message);
+        const actionPayload = formatAction(message);
+const r= await pause (20);    
+        const verificationPayload = {
+          petition_signature: {
+            subscribe_newsletter:
+              actionPayload.petition_signature.subscribe_newsletter,
+            data_handling_consent: handleConsent(message),
+          },
+        };
+        const data = await postAction(actionPayload);
+        if (data.petition_signature?.verification_token) {
+          const verified = await verification(
+            data.petition_signature.verification_token,
+            verificationPayload
+          );
+          return false; //true
+        } else {
+          return false;
+        }
+
+    return false;
+  };
+
+  fetchCampaign = async (campaign: ProcaCampaign): Promise<any> => {
+    console.log("fake fetching campaign", campaign.name);
+    return campaign;
+  };
+}
+
+export default new TrustCRM();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // to parse options
 import parseArg from "minimist";
 import dotenv from "dotenv";
+import { existsSync } from "fs";
 
 import { Configuration, DEFAULT_URL, help, configFromOptions } from "./config";
 import { listen } from "./listener";
@@ -10,34 +11,42 @@ export const main = (argv: string[]) => {
   const opt = parseArg(argv, {
     alias: { e: "env", v: "verbose" },
     default: { env: "" },
-    boolean: ["verbose", "dump", "help"],
-   "unknown": (param) => { 
-     if (param[0] === '-') {
-       console.error("invalid parameter",param);
-       process.exit(1);
-     }
-     return true;
-  }});
+    boolean: ["verbose", "dump", "help", "pause"],
+    unknown: (param) => {
+      if (param[0] === "-") {
+        console.error("invalid parameter", param);
+        process.exit(1);
+      }
+      return true;
+    },
+  });
 
-  
   let envConfig = undefined;
   if (opt.help) {
     //clihelp();
   }
 
   if (opt.env) {
-    envConfig = 
-    { path: opt.env }
+    if (!existsSync(opt.env)) {
+      const env = ".env." + opt.env;
+      if (existsSync(env)) opt.env = env;
+    } else {
+      console.error("missing env", opt.env);
+      process.exit(1);
+    }
+    envConfig = { path: opt.env };
   }
   const conf = dotenv.config(envConfig);
   if (process.env.SENTRY_URL) {
-     Sentry.init({dsn: process.env.SENTRY_URL});
+    Sentry.init({ dsn: process.env.SENTRY_URL });
   }
 
   try {
     const config = configFromOptions(conf);
     if (opt.dump) {
-      console.warn ("saving into data folder instead of using "+process.env.CRM);
+      console.warn(
+        "saving into data folder instead of using " + process.env.CRM
+      );
       process.env.CRM = "file";
     }
     console.log("listening for messages");
@@ -50,6 +59,5 @@ export const main = (argv: string[]) => {
 };
 
 if (require.main === module) {
-  main(process.argv.slice(2))
+  main(process.argv.slice(2));
 }
-
