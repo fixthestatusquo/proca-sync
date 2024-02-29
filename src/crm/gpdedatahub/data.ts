e19a878884f079919ef8275b131f91d11cd6d30d
1v4n4 <ivananl@yahoo.com>
2024-02-29 11:02:01 +0100

    Change promo code type to string

--- src/crm/gpdedatahub/data.ts
+++ src/crm/gpdedatahub/data.ts
@@ -1,9 +1,9 @@
 import type { ActionMessage } from "../../crm";
 
-export type GPAction = ActionMessage & { promo_code?: number };
+export type GPAction = ActionMessage & { promo_code?: string };
 
 export const formatAction = (queueAction: ActionMessage ) => {
   const data: GPAction = queueAction;
-  data.promo_code = data.contact?.phone ? 406750 : 406749;
+  data.promo_code = data.contact?.phone ? "406750" : "406749";
   return data;
 };
