850b672cea9141aeac22eaaccdee9405a8c16ad0
1v4n4 <ivananl@yahoo.com>
2024-03-20 09:25:47 +0100

    Handle widget promo code

--- src/crm/gpdedatahub/data.ts
+++ src/crm/gpdedatahub/data.ts
@@ -4,8 +4,14 @@ export type GPAction = ActionMessage & { promo_code?: string };
 
 export const formatAction = (queueAction: ActionMessage, config: any ) => {
   const data: GPAction = queueAction;
-  if (config?.component?.sync?.promoCode?.default.length > 0 || config?.component?.sync?.promoCode?.phone.length > 0) {
+  if (data.action.customFields.promocode || data.action.customFields.promocodephone) {
     data.promo_code = data.contact?.phone
+      ? data.action.customFields.promocodephone.toString() || ""
+      : data.action.customFields.promocode.toString() || "";
+  }
+  else if (config?.component?.sync?.promoCode?.default ||
+          config?.component?.sync?.promoCode?.phone) {
+      data.promo_code = data.contact?.phone
       ? config.component.sync.promoCode.phone || ""
       : config.component.sync.promoCode.default || ""
   }
