9e820fbbc9900c09fca03064a532397b60dfa49b
1v4n4 <ivananl@yahoo.com>
2026-03-23 22:36:23 +0100

    Replace sendinblue library with getBrevo

--- src/crm/sendinblue.ts
+++ src/crm/sendinblue.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import { BrevoClient } from "@getbrevo/brevo";
 import {
   CRM,
   CRMType,
@@ -6,21 +6,10 @@ import {
   type handleResult,
   type ProcaCampaign,
 } from "../crm";
-
-//import {ContactsApi, AccountApiApiKeys, CreateContact} from "@sendinblue/client";
-const SibApiV3Sdk = require("@sendinblue/client");
 import { string2map } from "../utils";
 
-/*
- *
-
-interface CRM {
-  fetchCampaigns = () : Promise<void>
-}
-*/
-
-class SendInBlueCRM extends CRM {
-  apiInstance = new SibApiV3Sdk.ContactsApi();
+class BrevoCRM extends CRM {
+  private brevo: BrevoClient;
   folderId = 0;
   mapping: undefined | Record<string, string>;
 
@@ -34,19 +23,16 @@ class SendInBlueCRM extends CRM {
       default:
         this.crmType = CRMType.OptIn;
     }
-    this.apiInstance.setApiKey(
-      SibApiV3Sdk.AccountApiApiKeys.apiKey,
-      process.env.SENDINBLUE_KEY,
-    );
+
+    this.brevo = new BrevoClient({
+      apiKey: process.env.SENDINBLUE_KEY || process.env.BREVO_KEY || "",
+    });
 
     const attributes = process.env.CONTACT_ATTRIBUTES;
     if (attributes !== undefined) {
       this.mapping = string2map(attributes as string);
     }
   }
-
-  //  mergeFields: any,
-  //  doubleOptIn = false,
 
   handleContact = async (
     message: ActionMessage,
@@ -58,67 +44,74 @@ class SendInBlueCRM extends CRM {
       console.log("failed fetching the campaign", message.campaign);
       return { processed: false };
     }
-    const createContact = new SibApiV3Sdk.CreateContact();
-    createContact.email = message.contact.email;
-    createContact.attributes = {};
+
+    const attributes: Record<string, any> = {};
+
     if (this.mapping) {
-      if (message.contact.phone)
+      if (message.contact.phone) {
         message.contact.phone = message.contact.phone.replaceAll(" ", "");
+      }
       for (const key in this.mapping) {
-        if (message.contact[key])
-          createContact.attributes[this.mapping[key]] = message.contact[key];
+        if (message.contact[key]) {
+          attributes[this.mapping[key]] = message.contact[key];
+        }
       }
     } else {
-      createContact.attributes = {
-        LANG: message.actionPage.locale,
-        FIRSTNAME: message.contact.firstName,
-        LASTNAME: message.contact.lastName || "",
-      };
-    }
-    if (process.env.OPTIN) {
-      createContact.attributes[process.env.OPTIN] = true;
-    }
-    if (process.env.DOUBLE_OPTIN) {
-      createContact.attributes[process.env.DOUBLE_OPTIN] = "Yes";
+      attributes.LANG = message.actionPage.locale;
+      attributes.FIRSTNAME = message.contact.firstName;
+      attributes.LASTNAME = message.contact.lastName || "";
     }
 
-    createContact.listIds = [camp.id];
-    if (process.env.LIST) {
-      createContact.listIds.push(parseInt(process.env.LIST, 10));
+    if (process.env.OPTIN) {
+      attributes[process.env.OPTIN] = true;
     }
-    createContact.emailBlacklisted = false;
-    createContact.updateEnabled = true;
-    console.log(createContact);
+    if (process.env.DOUBLE_OPTIN) {
+      attributes[process.env.DOUBLE_OPTIN] = "Yes";
+    }
+
+    const listIds = [camp.id];
+    if (process.env.LIST) {
+      listIds.push(parseInt(process.env.LIST, 10));
+    }
+
+    const payload = {
+      email: message.contact.email,
+      attributes,
+      listIds,
+      emailBlacklisted: false,
+      updateEnabled: true,
+    };
+
     try {
-      await this.apiInstance.createContact(createContact);
+      await this.brevo.contacts.createContact(payload);
+      console.log("contact added", message.contact.email);
+      return { processed: true };
     } catch (e) {
-      if (e.response.body) {
+      console.log(this.error(e));
+      if (e.body) {
         if (
-          e.response.body.code === "invalid_parameter" &&
-          e.response.body.message === "Invalid LANDLINE_NUMBER number"
+          e.body.code === "invalid_parameter" &&
+          e.body.message === "Invalid LANDLINE_NUMBER number"
         ) {
           try {
-            createContact.attributes["LANDLINE_NUMBER"] = undefined;
+            payload.attributes["LANDLINE_NUMBER"] = undefined;
+            await this.brevo.contacts.createContact(payload);
             console.error("Invalid phone number");
-            await this.apiInstance.createContact(createContact);
             return { processed: true };
           } catch (e) {
-            console.log(
-              "error creating even after removing phone",
-              e.response.body,
-            );
+            console.log("error creating even after removing phone", e.body);
             return { processed: false };
           }
         }
-        console.log("error creating", e.response.body);
+        console.log("error creating", e.body);
       } else {
         console.log("error creating no code", e);
       }
       //      const error = JSON.parse(e.body);
       //      console.log(error.code,error.message);
+
       return { processed: false };
     }
-    return { processed: true };
   };
 
   campaign = async (campaign: ProcaCampaign): Promise<Record<string, any>> => {
@@ -132,30 +125,28 @@ class SendInBlueCRM extends CRM {
     return Promise.resolve(this.campaigns[name]);
   };
 
-  fetchCampaigns = async () => {
+  fetchCampaigns = async (): Promise<void> => {
     try {
-      const folders = await this.apiInstance.getFolders(10, 0);
-      const name = "proca";
-      if (folders.body.folders) {
-        let procaFolder = folders.body.folders.filter(
-          (d: any) => d.name === name,
-        );
-        if (!procaFolder.length) {
-          const createFolder = new SibApiV3Sdk.CreateUpdateFolder();
-          createFolder.name = name;
-          const data = await this.apiInstance.createFolder(createFolder);
-          procaFolder = data.body;
-        } else {
-          procaFolder = procaFolder[0];
-        }
-        this.folderId = procaFolder.id;
+      const folders = await this.brevo.contacts.getFolders({
+        limit: 10,
+        offset: 0,
+      });
+      if (!folders.folders) return;
 
-        let lists = await this.apiInstance.getLists(50, 0);
-        lists = lists.body.lists;
-        if (lists.length) {
-          lists.forEach((d: any) => (this.campaigns[d.name] = d));
-        }
+      let procaFolder = folders.folders.find((d: any) => d.name === "proca");
+      if (!procaFolder) {
+        procaFolder = (await this.brevo.contacts.createFolder({
+          name: "proca",
+        })) as any;
       }
+
+      this.folderId = procaFolder!.id; // <-- non-null assertion
+
+      const lists = await this.brevo.contacts.getLists({
+        limit: 50,
+        offset: 0,
+      });
+      lists.lists?.forEach((d: any) => (this.campaigns[d.name] = d));
     } catch (e) {
       console.log("error fetching campaigns", e);
     }
@@ -164,11 +155,10 @@ class SendInBlueCRM extends CRM {
   fetchCampaign = async (campaign: ProcaCampaign): Promise<any> => {
     if (campaign.externalId) {
       try {
-        const data = await this.apiInstance.getList(campaign.externalId);
-        return data.body;
-      } catch (e) {
-        console.error("can't fetch list", campaign.externalId);
-        //        throw e; let's continue and fetch by name "normal"
+        const data = await this.brevo.contacts.getList(campaign.externalId);
+        return data;
+      } catch {
+        console.error("can't fetch list by externalId", campaign.externalId);
       }
     }
 
@@ -176,18 +166,18 @@ class SendInBlueCRM extends CRM {
       await this.fetchCampaigns();
       if (this.campaigns[campaign.name]) return this.campaigns[campaign.name];
     }
+
     try {
-      const createList = new SibApiV3Sdk.CreateList();
-
-      createList.name = campaign.name;
-      createList.folderId = this.folderId;
-
-      const data = await this.apiInstance.createList(createList);
-      console.log("fetching campaign " + campaign.name, data.body);
-      return data.body;
+      const data = await this.brevo.contacts.createList({
+        name: campaign.name,
+        folderId: this.folderId,
+      });
+      console.log("created list for campaign", campaign.name, data);
+      return data;
     } catch (e) {
-      console.log(e);
+      console.log("error creating list", e);
     }
   };
 }
-export default SendInBlueCRM;
+
+export default BrevoCRM;
